3448d3d76803cde03fbd8c0d882b23069c04d274
Ronaldo Dantas <ronaldo.dantas@polibrasnet.com.br>
2019-09-16 20:51:14 -0300

    NEW: Queues and appointment deletes

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'f639ad470abd98',
+    pass: '6fc41403e9f0c9',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -26,12 +26,14 @@ routes.put('/users', UserController.update);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/providers', ProviderController.index);
 
 routes.get('/schedule', ScheduleControler.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
